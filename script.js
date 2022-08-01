d1140c7d3a4dc3896648a531769d08c69fbd152e
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-08-01 15:21:43 +0100

    amendments

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // jQuery instructions begin with string $
 
-$("#circle").click(function() { // using string to select id and using click
+$("#circle").click(function() { // using string to select id and using click event to parse the following instruction
     alert("circle was clicked");
-});   
+}); 
+
+$(".square").click(function() {
+    alert("square was clicked");
+});
+
+$("div").click(function() {
+    alert("div was clicked");
+});
