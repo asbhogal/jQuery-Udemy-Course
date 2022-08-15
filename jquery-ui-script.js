d70c93eb1d1c2cc1e9c503741b9dc56f78402921
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-08-15 17:44:08 +0100

    added reset positions using jQuery

--- jquery-ui-script.js
+++ jquery-ui-script.js
@@ -1,9 +1,17 @@
 $("#draggable").draggable();
 $("#vertical-only-draggable").draggable({axis: "y"});   // SETS AN OPTION FOR DRAGGABLE TO ONLY OCCUR ON THE y AXIS
-$("#horizontal-only-draggable").draggable({axis: "x"});
+$("#horizontal-only-draggable").draggable({axis: "x"}); // SETS AN OPTION FOR DRAGGABLE TO ONLY OCCUR ON THE x AXIS
 
 $("#reset-btn").click(function () {
     $("#draggable").animate ({
+        top: "0px",
+        left: "0px"
+    });
+    $("#vertical-only-draggable").animate ({
+        top: "0px",
+        left: "0px"
+    });
+    $("#horizontal-only-draggable").animate ({
         top: "0px",
         left: "0px"
     });
