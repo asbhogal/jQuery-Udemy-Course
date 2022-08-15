cc822a0209d91f03fc5fabbf034d0c5328206561
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-08-15 17:42:42 +0100

    added draggable styling

--- jquery-ui-script.js
+++ jquery-ui-script.js
@@ -1,3 +1,10 @@
 $("#draggable").draggable();
-
 $("#vertical-only-draggable").draggable({axis: "y"});   // SETS AN OPTION FOR DRAGGABLE TO ONLY OCCUR ON THE y AXIS
+$("#horizontal-only-draggable").draggable({axis: "x"});
+
+$("#reset-btn").click(function () {
+    $("#draggable").animate ({
+        top: "0px",
+        left: "0px"
+    });
+});
