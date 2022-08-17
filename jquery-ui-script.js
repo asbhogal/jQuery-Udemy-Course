7f17fe67a5e2d9a18202815587dfc3a420741b81
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-08-17 16:37:51 +0100

    adjusted div positionings

--- jquery-ui-script.js
+++ jquery-ui-script.js
@@ -1,6 +1,11 @@
 $("#draggable").draggable();
-$("#vertical-only-draggable").draggable({axis: "y"});   // SETS AN OPTION FOR DRAGGABLE TO ONLY OCCUR ON THE y AXIS
-$("#horizontal-only-draggable").draggable({axis: "x"}); // SETS AN OPTION FOR DRAGGABLE TO ONLY OCCUR ON THE x AXIS
+$("#vertical-only-draggable").draggable({
+    axis: "y"                                       // SETS AN OPTION FOR DRAGGABLE TO ONLY OCCUR ON THE y AXIS
+});   
+
+$("#horizontal-only-draggable").draggable({
+    axis: "x"                                       // SETS AN OPTION FOR DRAGGABLE TO ONLY OCCUR ON THE x AXIS
+}); 
 
 $("#reset-btn").click(function () {
     $("#draggable").animate ({
@@ -22,4 +27,7 @@ $("#contained-draggable").draggable({containment: "#container"});   // PERMITS T
 // NB. A p TAG WILL ONLY MOVE UP AND DOWN IRRESPECTIVE OF WHETHER axis y IS ADDED DUE TO THE P STRETCHING THE ENTIRE WIDTH OF ITS PARENT CONTAINER
 // CHANGING THE p TAG TO A SPAN WILL RESOLVE THE ABOVE AND ALLOW FREE MOVEMENT ACROSS x AND y AXIS
 
-$("#resizable-div").resizable();    // ENABLES THE DIV TO BE RESIZED FREELY ALONG THE x AND y AXIS
+$("#resizable-div").resizable();                // ENABLES THE DIV TO BE RESIZED FREELY ALONG THE x AND y AXIS
+$("#grid-resizable-div").resizable({            // 'SNAPS' THE RESIZE TO 50px WIDTH ALONG THE x AND y AXIS
+    grid: 50
+});
